a0e3cf2ee2e9e3ac473d6f82c14f2ee52abb491b
ramicortes <ramirocortes7@gmail.com>
2020-04-24 13:11:00 -0300

    Added preference-creation endpoint, added modal to details view and added back_url views

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ require('./config/config');
 var express = require('express');
 var exphbs = require('express-handlebars');
 const mercadopago = require('mercadopago');
+const bodyParser = require('body-parser');
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // configuracion de handlebars
 app.engine('handlebars', exphbs());
@@ -16,11 +23,20 @@ app.get('/', function(req, res) {
 app.get('/detail', function(req, res) {
     res.render('detail', req.query);
 });
+app.get('/approved', function(req, res) {
+    res.render('approved');
+});
+app.get('/rejected', function(req, res) {
+    res.render('rejected');
+});
+app.get('/pending', function(req, res) {
+    res.render('pending');
+});
 app.use(require('./routes/index'));
 
 // configuración de mercadopago
 mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN
+    access_token: process.env.ACCESS_TOKEN_PROD
 });
 
 app.use(express.static('assets'));
--- routes/checkout.js
+++ routes/checkout.js
@@ -2,12 +2,73 @@ const express = require('express');
 const mercadopago = require('mercadopago');
 const app = express();
 
+app.post('/checkout/preference', (req, res) => {
+    console.log('payment');
+    let preference = {
+        items: [{
+            id: '1234',
+            title: req.body.title,
+            description: 'Dispositivo móvil de tienda e-commerce',
+            picture_url: req.body.picture,
+            unit_price: Number(req.body.price) || 0,
+            quantity: 1,
+        }],
+        payer: {
+            name: 'Lalo',
+            surname: 'Landa',
+            email: 'test_user_63274575@testuser.com',
+            phone: {
+                area_code: '011',
+                number: 22223333
+            },
+            identification: {
+                type: 'DNI',
+                number: '22.333.444'
+            },
+            address: {
+                zip_code: '1111',
+                street_name: 'Falsa',
+                street_number: 123
+            }
+        },
+        payment_methods: {
+            excluded_payment_methods: [{
+                id: 'amex'
+            }],
+            excluded_payment_types: [{
+                id: 'amt'
+            }],
+            installments: 6
+        },
+        back_urls: {
+            success: 'localhost:3000/approved',
+            pending: 'localhost:3000/pending',
+            failure: 'localhost:3000/rejected'
+        },
+        notification_url: 'localhost:3000/checkout/webhook',
+        auto_return: 'approved',
+        external_reference: 'ABCD1234'
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(function(response) {
+            // Este valor reemplazará el string "$$init_point$$" en tu HTML
+            global.init_point = response.body.init_point;
+            res.json(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
+
 app.post('/checkout/payment', (req, res) => {
-    console.log(mercadopago);
-    res.json('Aca se paga');
+    console.log('hola');
+    console.log(req);
 });
 
 app.post('/checkout/webhook', (req, res) => {
+    console.log("hay retorno");
+    console.log(req);
     res.json('Aca se reciben notificaciones');
 });
 
